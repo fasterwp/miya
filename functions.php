df5b505116308f19db13fe92b26c313b14e61b56
Andrei Chira <andreichira777@gmail.com>
2017-11-28 22:23:01 +0200

    naming

--- functions.php
+++ functions.php
@@ -41,15 +41,15 @@ include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.
 include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
 
 // Child theme (do not remove).
-define( 'CHILD_THEME_NAME', 'Miya' );
-define( 'CHILD_THEME_URL', 'https://fasterwp.com/' );
+define( 'CHILD_THEME_NAME', 'Genesis Sample' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.3.0' );
 
 // Enqueue Scripts and Styles.
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Old+Standard+TT|Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
@@ -113,12 +113,8 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 // Add Image Sizes.
 add_image_size( 'featured-image', 720, 400, TRUE );
 
-//* Reposition the primary navigation menu
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_header', 'genesis_do_nav', 12 );
-
 // Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
 
 // Reposition the secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
@@ -170,7 +166,7 @@ add_action( 'pre_get_posts', 'sk_change_archives_posts_per_page' );
 function sk_change_archives_posts_per_page( $query ) {
 
 	if( $query->is_main_query() && !is_admin() && !is_singular() && !is_search() ) {
-		$query->set( 'posts_per_page', '9' );
+		$query->set( 'posts_per_page', '8' );
 	}
 
 }
